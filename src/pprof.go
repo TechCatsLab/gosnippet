0f30ae7a635b63fbf665f457e912bb81d53c7bb2
Txiaozhe <t_xiaoji@qq.com>
2017-04-05 20:23:21 +0800

    profile

---
+++ src/pprof.go
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/04/05        Tang Xiaoji
+ */
+
+package main
+
+/**
+net/http/pprof方式进行性能分析
+ */
+import (
+	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func main() {
+	go func() {
+		for {
+			fmt.Println("hello...")
+		}
+	}()
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
+
+//http://127.0.0.1:8080/debug/pprof 查看整体信息
+//http://127.0.0.1:8080/debug/pprof/profile 可以将cpu profile下载下来观察分析
+//
+//从terminal进入profile进行细致分析
+//go tool pprof http://localhost:6060/debug/pprof/profile
+//go tool pprof http://localhost:6060/debug/pprof/heap
+//go tool pprof http://localhost:6060/debug/pprof/block
---
+++ src/profile.go
@@ -0,0 +1,90 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/04/05        Tang Xiaoji
+ */
+
+package main
+
+import (
+	"os"
+	"log"
+	"runtime/pprof"
+	"context"
+	"time"
+	"fmt"
+)
+
+func main() {
+	cpuf, err := os.Create("cpu_profile")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pprof.StartCPUProfile(cpuf)
+	defer pprof.StopCPUProfile()
+
+	ctx, _ := context.WithTimeout(context.Background(), time.Second * 5)
+	test(ctx)
+
+	time.Sleep(time.Second * 3)
+
+	memf, err := os.Create("mem_profile")
+	if err != nil {
+		log.Fatal("could not create memory profile: ", err)
+	}
+	if err := pprof.WriteHeapProfile(memf); err != nil {
+		log.Fatal("could not write memory profile: ", err)
+	}
+	memf.Close()
+}
+
+func test(c context.Context) {
+	i := 0
+	j := 0
+
+	go func() {
+		m := map[int]int{}
+		for {
+			i++
+			m[i] = i
+		}
+	}()
+
+	go func() {
+		m := map[int]int{}
+		for {
+			j++
+			m[i] = i
+		}
+	}()
+
+	select {
+	case <- c.Done() :
+		fmt.Println("DONE, i:", i, " j:", j)
+		return
+	}
+}
