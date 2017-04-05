88ad131f7ba57d262727d9454e4033b9cf9c6a5b
yangchenglong11 <2139185304@qq.com>
2017-04-05 22:03:58 +0800

    pool示例

---
+++ src/pool.go
@@ -0,0 +1,68 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/04/05        Yang Chenglong
+ */
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	// 禁用GC，并保证在main函数执行结束前恢复GC
+	defer debug.SetGCPercent(debug.SetGCPercent(-1))
+	var count int32
+	newFunc := func() interface{} {
+		return atomic.AddInt32(&count, 1)
+	}
+
+	pool := sync.Pool{New: newFunc}
+
+	// New 字段值的作用
+	v1 := pool.Get()
+	fmt.Printf("v1: %v\n", v1)
+
+	// 临时对象池的存取
+	pool.Put(newFunc())
+	pool.Put(newFunc())
+	pool.Put(newFunc())
+	v2 := pool.Get()
+	fmt.Printf("v2: %v\n", v2)
+
+	// 垃圾回收对临时对象池的影响
+	debug.SetGCPercent(100)
+	runtime.GC()
+	v3 := pool.Get()
+	fmt.Printf("v3: %v\n", v3)
+	pool.New = nil
+	v4 := pool.Get()
+	fmt.Printf("v4: %v\n", v4)
+}
