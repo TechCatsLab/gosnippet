5ddda8be53d3338f36540149c6220ff56e4ccc79
Feng Yifei <fengyfei@gmail.com>
2017-04-14 11:52:23 +0800

    slice 比较代码.
    cd bench;
    go test -bench=.

---
+++ src/bench/slice_test.go
@@ -0,0 +1,75 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/04/14        Feng Yifei
+ */
+
+package bench
+
+import (
+	"reflect"
+	"testing"
+)
+
+func stringSliceReflectEqual(a, b []string) bool {
+	return reflect.DeepEqual(a, b)
+}
+
+func stringSliceEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func BenchmarkEqual(b *testing.B) {
+	sa := []string{"q", "w", "e", "r", "t"}
+	sb := []string{"q", "w", "a", "s", "z", "x"}
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		stringSliceEqual(sa, sb)
+	}
+}
+
+func BenchmarkDeepEqual(b *testing.B) {
+	sa := []string{"q", "w", "e", "r", "t"}
+	sb := []string{"q", "w", "a", "s", "z", "x"}
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		stringSliceReflectEqual(sa, sb)
+	}
+}
