dcb0efe28067b470b8f673a4db5b8a3226f74cfc
Feng Yifei <fengyfei@gmail.com>
2017-04-19 15:02:31 +0800

    工厂模式.

---
+++ src/design/creation/factoryMethod.go
@@ -0,0 +1,81 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/04/19        Feng Yifei
+ */
+
+/**
+ * 工厂方法模式：
+ *     定义一个用于创建对象的接口，让子类决定将哪一个类实例化
+ * 特点：
+ *     工厂方法模式让一个类的实例化延迟到其子类
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+// 抽象工厂方法接口
+type Factory interface {
+	factoryMethod() Product
+}
+
+// 要调用的抽象方法
+type Product interface {
+	method()
+}
+
+// 对象创建者
+type Creator struct {
+	factory Factory
+}
+
+// 对外暴露的接口
+func (c *Creator) Operation() {
+	product := c.factory.factoryMethod()
+	product.method()
+}
+
+// 具体工厂创建具体产品，且只创建一种产品
+type ConcreteCreator struct{}
+
+func (c *ConcreteCreator) factoryMethod() Product {
+	return new(ConcreteProduct)
+}
+
+// 具体产品
+type ConcreteProduct struct{}
+
+func (p *ConcreteProduct) method() {
+	fmt.Println("ConcreteProduct.method()")
+}
+
+func main() {
+	creator := Creator{new(ConcreteCreator)}
+	creator.Operation()
+}
---
+++ src/design/creation/simpleFactory.go
@@ -0,0 +1,88 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/04/19        Feng Yifei
+ */
+
+/**
+ * 简单工厂模式：
+ *     定义一个工厂类，根据传入的参数不同返回不同的实例，被创建的实例具有共同的父类或接口
+ * 特点：
+ *     需要创建的对象不多
+ *     对象种类不会频繁变更
+ *     模块不关注对象创建过程
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+// 接口
+type Shape interface {
+	Draw()
+}
+
+// Circle
+type Circle struct {}
+
+func (this *Circle) Draw() {
+	fmt.Println("Circle drawing")
+}
+
+// Rectangle
+type Rectangle struct {}
+
+func (this *Rectangle) Draw() {
+	fmt.Println("Rectangle drawing")
+}
+
+// Simple Factory
+type SimpleFactory struct{}
+
+func (this *SimpleFactory) CreateShape(shape string) Shape {
+	var (
+		result	Shape
+	)
+
+	switch shape {
+	case "circle":
+		result = &Circle{}
+
+	case "rectangle":
+		result = &Rectangle{}
+	}
+
+	return result
+}
+
+func main() {
+	factory := &SimpleFactory{}
+
+	factory.CreateShape("circle").Draw()
+	factory.CreateShape("rectangle").Draw()
+}
