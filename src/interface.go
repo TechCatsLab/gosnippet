aea10b78070429d5b76eb29ef0dabda9101551e0
Feng Yifei <fengyfei@gmail.com>
2017-04-19 15:30:08 +0800

    接口示例.

---
+++ src/interface.go
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/04/19        Feng Yifei
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+type interfaceI interface {
+	method()
+	add()
+}
+
+type interfaceII interface {
+	method()
+	sub()
+}
+
+type conflict struct{}
+
+func (this *conflict) method() {
+	fmt.Println("method()")
+}
+
+func (this *conflict) sub() {
+	fmt.Println("sub()")
+}
+
+func main()  {
+	var (
+		c	interface{}
+		ok	bool
+		i	interfaceI
+		ii	interfaceII
+	)
+
+	c = &conflict{}
+
+	if i, ok = c.(interfaceI); ok {
+		i.method()
+	}
+
+	if ii, ok = c.(interfaceII); ok {
+		ii.sub()
+	}
+}
