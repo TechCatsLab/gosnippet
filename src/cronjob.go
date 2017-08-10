33e0184995e7345dbf9e9451970229f6a8dab9ab
yusank <yusankurban@gmail.com>
2017-08-10 21:43:50 +0800

    添加 crontime

---
+++ src/cronjob.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/robfig/cron"
+)
+
+func main() {
+	c := cron.New()
+	// cronTime := "* * * * * Mon,Wed" 表示星期一，星期三执行
+	// cronTime := "0 0 19 * * *" 每天 19:00 点执行一次
+	// cronTime := "* * 8-16 * * *" 表示 8am 到 4pm 整点执行（包括8和16）
+	cronTime := "* */5 * * * *" // 每五分钟执行一次
+
+	c.AddFunc(cronTime, func() {
+		// Do want you want
+		fmt.Print(time.Now())
+	})
+	c.Start
+
+	// 确保函数不会跳出
+	select {}
+}
