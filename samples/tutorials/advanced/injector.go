83450994d6833259eb05aede69f6f3f9a2028319
Feng Yifei <fengyfei@gmail.com>
2018-02-11 14:12:35 +0800

    injector sample

---
+++ samples/tutorials/advanced/injector.go
@@ -0,0 +1,61 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 SmartestEE Co. Ltd..
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2018/02/11        Feng Yifei
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+type target struct{}
+
+func (t *target) Run() {
+	fmt.Println("target method Run")
+}
+
+func (t *target) RunNow() {
+	fmt.Println("target method RunNow")
+}
+
+type injector struct {
+	*target
+}
+
+func (i *injector) Run() {
+	i.RunNow()
+
+	fmt.Println("injector Run")
+}
+
+func main() {
+	t := &target{}
+	s := &injector{t}
+
+	s.Run()
+}
